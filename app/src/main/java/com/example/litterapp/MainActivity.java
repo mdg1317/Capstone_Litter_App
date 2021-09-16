718fba2f0c09d8948801a376a047673d5fa9dc20
mdg1317 <matthew.dennis137@gmail.com>
2021-09-16 14:31:02 -0700

    Added camera and menu buttons for demo

--- app/src/main/java/com/example/litterapp/MainActivity.java
+++ app/src/main/java/com/example/litterapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.litterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ConstraintLayout layout = findViewById(R.id.layout);
+        Button buttonMenu = findViewById(R.id.buttonMenu);
+        ImageButton buttonCamera = findViewById(R.id.buttonCamera);
+
+        // change background color to red when clicking menu button
+        // TEMPORARY
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view){
+                layout.setBackgroundColor(Color.RED);
+            }
+        });
+
+        // change background color to green when clicking camera button
+        // TEMPORARY
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view){
+                layout.setBackgroundColor(Color.GREEN);
+            }
+        });
+
+        // change background color to white when clicking on main screen
+        // TEMPORARY
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view){
+                layout.setBackgroundColor(Color.WHITE);
+            }
+        });
     }
 }
