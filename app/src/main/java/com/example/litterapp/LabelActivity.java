a017541214e734f39784bedcf6296a3a2502c5b0
co22 <co22@up.edu>
2021-10-08 05:22:39 -0700

    Edited issues caused by merging

--- app/src/main/java/com/example/litterapp/LabelActivity.java
+++ app/src/main/java/com/example/litterapp/LabelActivity.java
@@ -18,19 +18,22 @@ public class LabelActivity extends AppCompatActivity {
 
         final ConstraintLayout layout = findViewById(R.id.leaderboards_text);
         Button buttonBack = findViewById(R.id.button_back);
+        Button buttonFinish = findViewById(R.id.button_finish);
 
         // Go back to main activity when back button is pressed
         //buttonBack.setOnClickListener(view -> setContentView(R.layout.activity_main));
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
-<<<<<<< HEAD
             public void onClick(View v) {
                 finish();
-=======
-            public void onClick( View view){
-                setContentView(R.layout.activity_menu);
+            }
+        });
 
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
