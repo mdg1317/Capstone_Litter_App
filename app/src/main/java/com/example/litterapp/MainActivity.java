3c89da09c1165569d57c6d619d449a0521dabc6e
mdg1317 <matthew.dennis137@gmail.com>
2021-10-28 21:34:18 -0700

    Testing Flask server connection

--- app/src/main/java/com/example/litterapp/MainActivity.java
+++ app/src/main/java/com/example/litterapp/MainActivity.java
@@ -11,8 +11,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private FusedLocationProviderClient fusedLocationClient;
+
+    private String url = "http://" + "10.0.2.2" + ":" + 5000 + "/";
+    private String postBodyString;
+    private MediaType mediaType;
+    private RequestBody requestBody;
+    private Button connectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
         final ConstraintLayout layout = findViewById(R.id.leaderboards_text);
         Button buttonMenu = findViewById(R.id.buttonMenu);
         ImageButton buttonCamera = findViewById(R.id.buttonCamera);
+
+        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        connectButton = findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                postRequest("your message here", url);
+            }
+        });
 
         // transition to menu screen when clicking on menu button
         buttonMenu.setOnClickListener(new View.OnClickListener() {
@@ -83,5 +112,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
         popupMenu.show();
+    }
+
+    private RequestBody buildRequestBody(String msg) {
+        postBodyString = msg;
+        mediaType = MediaType.parse("text/plain");
+        requestBody = RequestBody.create(postBodyString, mediaType);
+        return requestBody;
+    }
+
+    private void postRequest(String message, String URL) {
+        RequestBody requestBody = buildRequestBody(message);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request
+                .Builder()
+                .post(requestBody)
+                .url(URL)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        call.cancel();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_LONG).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
