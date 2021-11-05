0ab152536682284a0c6ab47413b2178385e9161c
Logan Crawford <crawforl20@up.edu>
2021-11-05 03:35:31 -0700

    Style & image & Format changes

--- app/src/main/java/com/example/litterapp/LeaderboardsActivity.java
+++ app/src/main/java/com/example/litterapp/LeaderboardsActivity.java
@@ -2,12 +2,19 @@ package com.example.litterapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LeaderboardsActivity extends AppCompatActivity {
+
+    Button showMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,13 @@ public class LeaderboardsActivity extends AppCompatActivity {
                 finish();
             }
         });
-    }
 
+        //get spinner from xml file.
+        Spinner dropdown = findViewById(R.id.spinner1);
+        //create list of regions for spinner
+        String[] items = new String[]{"Americas", "Asia", "Europe", "Global", "United States"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
 }
