80fe0ee3a9734106a3c647ad768e68677746645c
co22 <co22@up.edu>
2022-03-15 16:19:04 -0700

    -added pet activity

---
+++ app/src/main/java/com/example/litterapp/PetActivity.java
@@ -0,0 +1,20 @@
+package com.example.litterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
